61d63f9143eb60774c66b9e3f1da89776fe26f5a
BA8595 <161141899+BA8595@users.noreply.github.com>
2024-02-24 17:34:20 +0000

    containerizing index.html

---
+++ NodeApp/NodeApp/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+ res.send('Hello World from NodeApp! docker is cool!');
+ }
+);
+app.listen(3000, () => {
+ console.log('Server is up on 3000');
+ }
+);
